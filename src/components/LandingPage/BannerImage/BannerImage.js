77e8606cb8aeb9f46a3d0c28f67095745f3899ac
SUJAY GOWDA <sujay.gowda@i4sight.net>
2023-08-12 11:03:27 +0530

    LandingPage section updated

--- src/components/LandingPage/BannerImage/BannerImage.js
+++ src/components/LandingPage/BannerImage/BannerImage.js
@@ -1,10 +1,14 @@
-import styles from "./BannerImage.module.css";
+import classes from "./BannerImage.module.css";
 import bannerImage from "../../../assets/banner_image.jpg";
 
 export default function BannerImage() {
   return (
-    <div className={styles["image-container"]}>
-      <img src={bannerImage} alt="sujay gowda" />
+    <div className={classes["image-container"]}>
+      <img
+        className={classes["banner-image"]}
+        src={bannerImage}
+        alt="sujay gowda"
+      />
     </div>
   );
 }
